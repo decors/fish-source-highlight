596760aba6e56adaf25d1a13e58c5f762515a8b7
Decors <decors1215@gmail.com>
2016-06-10 15:03:35 +0900

    linux version more

--- functions/more.fish
+++ functions/more.fish
@@ -1,5 +1,9 @@
 function more -d "opposite of more"
-    set -lx MORE '-R'
-    type -q source-highlight; and set -lx LESSOPEN '| src-hilite-lesspipe %s'
-    command more $argv
+    if command more --version >/dev/null ^/dev/null
+        set -lx MORE '-R'
+        type -q source-highlight; and set -lx LESSOPEN '| src-hilite-lesspipe %s'
+        command more $argv
+    else
+        command more $argv
+    end
 end
