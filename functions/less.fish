fe0670357f6f09bc47ecc444fc9fb2acd2f64f86
Decors <decors1215@gmail.com>
2019-03-11 16:48:30 +0900

    Add environment variable #1
    * LESSOPEN needs $SHELL

--- functions/less.fish
+++ functions/less.fish
@@ -1,4 +1,5 @@
 function less
+    set -lx SHELL (which fish)
     set -lx LESS '-R'
     type -q src-hilite-lesspipe; and set -lx LESSOPEN '| src-hilite-lesspipe %s'
     command less $argv
--- functions/more.fish
+++ functions/more.fish
@@ -1,5 +1,6 @@
 function more
-    if echo | command more -R >/dev/null ^/dev/null
+    if echo | command more -R >/dev/null 2>/dev/null
+        set -lx SHELL (which fish)
         set -lx MORE '-R'
         type -q src-hilite-lesspipe; and set -lx LESSOPEN '| src-hilite-lesspipe %s'
         command more $argv
