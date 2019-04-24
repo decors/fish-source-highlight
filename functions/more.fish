f5b629314a3dc939cc31af826d3a39f1cd5ca954
Decors <decors1215@gmail.com>
2019-04-24 12:36:17 +0900

    Fix `more` command confirmation at once

--- functions/more.fish
+++ functions/more.fish
@@ -1,10 +1,8 @@
-function more
-    if echo | command more -R >/dev/null 2>/dev/null
+if echo | command more -R >/dev/null 2>&1
+    function more
         set -lx SHELL (which fish)
         set -lx MORE '-R'
         type -q src-hilite-lesspipe; and set -lx LESSOPEN '| src-hilite-lesspipe %s'
-        command more $argv
-    else
         command more $argv
     end
 end
