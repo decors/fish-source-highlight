f8cc8cbbb804bf1c2c5b6feb261f95d77ae978cf
Decors <decors1215@gmail.com>
2019-04-25 13:58:33 +0900

    Rename func

--- functions/__source-highlight.fish
+++ functions/__source-highlight.fish
@@ -1,4 +1,4 @@
-function source-highlight
+function __source-highlight
     test -f $source_highlight_style_file;
         and set stylefile $source_highlight_style_file;
         or set stylefile esc.style
--- functions/src-hilite-lesspipe.fish
+++ functions/src-hilite-lesspipe.fish
@@ -2,9 +2,9 @@ function src-hilite-lesspipe
     for source in $argv
         switch "$source"
             case "*ChangeLog" "*changelog"
-                source-highlight --failsafe -f esc --lang-def=changelog.lang -i "$source"
+                __source-highlight --failsafe -f esc --lang-def=changelog.lang -i "$source"
             case "Makefile" "*makefile"
-                source-highlight --failsafe -f esc --lang-def=makefile.lang -i "$source"
+                __source-highlight --failsafe -f esc --lang-def=makefile.lang -i "$source"
             case "*.tar" "*.tgz" "*.gz" "*.bz2" "*.xz" "*.zip"
                 if type -qf lesspipe.sh
                     command lesspipe.sh "$source"
@@ -12,7 +12,7 @@ function src-hilite-lesspipe
                     command lesspipe "$source"
                 end
             case "*"
-                source-highlight --failsafe --infer-lang -f esc -i "$source"
+                __source-highlight --failsafe --infer-lang -f esc -i "$source"
         end
     end
 end
